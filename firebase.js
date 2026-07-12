9c31e4a8ac0724460d441f4dc64a1b41f33acd45
safaamhasan99-ship-it <safaa.mhasan99@gmail.com>
2026-07-12 21:07:20 +0300

    Set up Firebase configuration and imports
    Initialized Firebase app with Firestore, Storage, and Auth.

---
+++ firebase.js
@@ -0,0 +1,29 @@
+// استيراد Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+  getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// ضع بيانات مشروع Firebase هنا
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
