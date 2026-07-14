1df786b528d9fb730acd06486853c524e9ec9b8a
safaamhasan99-ship-it <safaa.mhasan99@gmail.com>
2026-07-14 03:26:07 +0300

    Refactor Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,28 +1,76 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// firebase.js
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+import { getStorage } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+// إعدادات مشروع Firebase
 
 const firebaseConfig = {
+
+
   apiKey: "AIzaSyCcaOIwNodH5IESCShyYQkpHBFiywzIi-4",
+
+
   authDomain: "hamza-shatri-store.firebaseapp.com",
+
+
   projectId: "hamza-shatri-store",
+
+
   storageBucket: "hamza-shatri-store.firebasestorage.app",
+
+
   messagingSenderId: "372483512160",
+
+
   appId: "1:372483512160:web:b594dd13f4774db6d13005"
+
+
 };
+
+
+
+// تشغيل Firebase
 
 const app = initializeApp(firebaseConfig);
 
+
+
+// الخدمات
+
 const db = getFirestore(app);
+
+
 const storage = getStorage(app);
+
+
 const auth = getAuth(app);
 
+
+
+// تصدير الاستخدام
+
 export {
+
   db,
+
   storage,
+
   auth
+
 };
